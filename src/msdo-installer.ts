62416f8fe3c9aab67863ae4c3326ac5a2d0edc73
David Knise <daknis@microsoft.com>
2023-06-13 12:09:40 -0700

    release/vNext (#10)
    * Change to use REST API to install MSDO CLI
    
    * Updated msdo-client.ts
    
    * Remove and ignore lib folder, as it's confusing to checkin build output unless it's the github action.
    
    * Delete lib folder
    
    * Additional updates with flatter no-class js style
    
    * try publishing a package
    
    * Update the readme
    
    * v1.7.0 - udpate nuget downloadFile retry logic
    
    * v1.7.0-beta.3
    
    * v1.7.0-beta.4
    
    * v1.7.0-beta.5
    
    * v1.7.0 continue to iterate on decompress response
    
    * v1.7.0-beta.6 - fix decompress-response import
    
    * v1.7.0-beta.7 - readd decompress-response dependency
    
    * v1.7.0 - working release
    
    * v1.7.0 - update changelog
    
    * v1.7.0 - fix typo in changelog.
    
    ---------
    
    Co-authored-by: Jiandong Jiang <jianj@microsoft.com>

--- src/msdo-installer.ts
+++ src/msdo-installer.ts
@@ -2,267 +2,177 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as process from 'process';
 import * as tl from 'azure-pipelines-task-lib/task';
+import * as common from './msdo-common';
+import * as nuget from './msdo-nuget-client';
 
-export class MsdoInstaller {
+/**
+ * Installs the Microsoft Security DevOps (MSDO) CLI 
+ * 
+ * @param cliVersion - The version of the MSDO CLI to install. Also accepts 'latest' or 'latestprerelease' values.
+ */
+export async function install(cliVersion: string): Promise<void> {
+    console.log(`Installing Microsoft Security DevOps Cli version: ${cliVersion}`);
 
-    async install(cliVersion: string) {
-        console.log('Installing Microsoft Security DevOps Cli...');
+    if (process.env.MSDO_FILEPATH) {
+        console.log(`MSDO CLI File Path overriden by %MSDO_FILEPATH%: ${process.env.MSDO_FILEPATH}`);
+        return;
+    }
 
-        if (process.env.MSDO_FILEPATH) {
-            console.log(`MSDO CLI File Path overriden by %MSDO_FILEPATH%: ${process.env.MSDO_FILEPATH}`);
-            return;
-        }
+    if (process.env.MSDO_DIRECTORY) {
+        console.log(`MSDO CLI Directory overriden by %MSDO_DIRECTORY%: ${process.env.MSDO_DIRECTORY}`);
 
-        if (process.env.MSDO_DIRECTORY) {
-            console.log(`MSDO CLI Directory overriden by %MSDO_DIRECTORY%: ${process.env.MSDO_DIRECTORY}`);
+        // Set the msdo file path
+        let msdoFilePath = path.join(process.env.MSDO_DIRECTORY, 'guardian');
+        tl.debug(`msdoFilePath = ${msdoFilePath}`);
 
-            // Set the msdo file path
-            let msdoFilePath = path.join(process.env.MSDO_DIRECTORY, 'guardian');
-            tl.debug(`msdoFilePath = ${msdoFilePath}`);
+        process.env.MSDO_FILEPATH = msdoFilePath;
+        return;
+    }
 
-            process.env.MSDO_FILEPATH = msdoFilePath;
-            return;
-        }
+    let packageName = resolvePackageName();
 
-        // initialize the _msdo directory
-        let agentDirectory = path.join(process.env.AGENT_ROOTDIRECTORY, '_msdo');
-        tl.debug(`agentDirectory = ${agentDirectory}`);
-        this.ensureDirectory(agentDirectory);
+    // initialize the _msdo directory
+    let agentDirectory = path.join(process.env.AGENT_ROOTDIRECTORY, '_msdo');
+    tl.debug(`agentDirectory = ${agentDirectory}`);
+    common.ensureDirectory(agentDirectory);
 
-        let agentPackagesDirectory = process.env.MSDO_PACKAGES_DIRECTORY;
-        if (!agentPackagesDirectory) {
-            agentPackagesDirectory = path.join(agentDirectory, 'packages');
-            tl.debug(`agentPackagesDirectory = ${agentPackagesDirectory}`);
-            this.ensureDirectory(agentPackagesDirectory);
-            process.env.MSDO_PACKAGES_DIRECTORY = agentPackagesDirectory;
-        }
+    let agentPackagesDirectory = process.env.MSDO_PACKAGES_DIRECTORY;
+    if (!agentPackagesDirectory) {
+        agentPackagesDirectory = path.join(agentDirectory, 'packages');
+        tl.debug(`agentPackagesDirectory = ${agentPackagesDirectory}`);
+        common.ensureDirectory(agentPackagesDirectory);
+        process.env.MSDO_PACKAGES_DIRECTORY = agentPackagesDirectory;
+    }
 
-        let agentVersionsDirectory = path.join(agentDirectory, 'versions');
-        tl.debug(`agentVersionsDirectory = ${agentVersionsDirectory}`);
-        this.ensureDirectory(agentVersionsDirectory);
+    let agentVersionsDirectory = path.join(agentDirectory, 'versions');
+    tl.debug(`agentVersionsDirectory = ${agentVersionsDirectory}`);
+    common.ensureDirectory(agentVersionsDirectory);
 
-        let msdoVersionsDirectory = path.join(agentVersionsDirectory, 'microsoft.security.devops.cli');
-        tl.debug(`msdoVersionsDirectory = ${msdoVersionsDirectory}`);
+    if (isInstalled(agentVersionsDirectory, packageName, cliVersion)) {
+        return;
+    }
 
-        if (this.isInstalled(msdoVersionsDirectory, cliVersion)) {
-            return;
-        }
+    let failed = false;
+    let attempts = 0;
+    let maxAttempts = 2;
 
-        let failed = false;
-        let attempts = 0;
-        let maxAttempts = 2;
+    let serviceIndexUrl = "https://api.nuget.org/v3/index.json";
+    let response: nuget.InstallNuGetPackageResponse;
 
-        do {
-            try {
-                failed = false;
+    do {
+        failed = false;
 
-                const msdoTaskLibDirectory = path.resolve(__dirname);
-                tl.debug(`msdoTaskLibDirectory = ${msdoTaskLibDirectory}`);
-
-                const msdoProjectFile = path.join(msdoTaskLibDirectory, 'msdo-task-lib.proj');
-                tl.debug(`msdoProjectFile = ${msdoProjectFile}`);
-
-                let tool = tl.tool('dotnet')
-                    .arg('restore')
-                    .arg(msdoProjectFile)
-                    .arg(`/p:MsdoPackageVersion=${cliVersion}`)
-                    .arg('--packages')
-                    .arg(agentVersionsDirectory)
-                    .arg('--source')
-                    .arg('https://api.nuget.org/v3/index.json');
-
-                await tool.exec();
-            } catch (error) {
-                tl.debug(error);
-                failed = true;
-                attempts += 1;
-                if (attempts > maxAttempts) {
-                    break;
-                }
+        try {
+            response = await nuget.install(
+                serviceIndexUrl,
+                packageName,
+                cliVersion,
+                agentVersionsDirectory);
+        } catch (error) {
+            tl.debug(error);
+            failed = true;
+            attempts += 1;
+            if (attempts > maxAttempts) {
+                break;
             }
-        } while (failed);
+        }
+    } while (failed);
 
-        this.resolvePackageDirectory(msdoVersionsDirectory, cliVersion);
+    if (response && response.success) {
+        if (response.inCache == true) {
+            console.log(`${packageName} version ${response.resolvedVersion} already installed`);
+        } else {
+            console.log(`Installed ${packageName} version ${response.resolvedVersion}`);
+        }
+    } else {
+        throw new Error('Failed to install the MSDO CLI nuget package.');
     }
 
-    ensureDirectory(directory: string) : void {
-        if (!fs.existsSync(directory)) {
-            fs.mkdirSync(directory);
+    setVariables(agentVersionsDirectory, packageName, response.resolvedVersion, true);
+}
+
+/**
+ * Resolves the name of the Guardian CLI package to install based on the current platform
+ * 
+ * @returns the name of the Guardian CLI package to install
+ */
+function resolvePackageName(): string {
+    let packageName: string;
+    if (process.env.MSDO_DOTNETDEPENDENTPACKAGE) {
+        packageName = 'Microsoft.Security.Devops.Cli';
+    } else if (process.platform == 'win32') {
+        packageName = 'Microsoft.Security.Devops.Cli.win-x64';
+    } else if (process.platform == 'linux') {
+        if (process.arch == 'arm64') {
+            packageName = 'Microsoft.Security.Devops.Cli.linux-arm64';
+        } else {
+            packageName = 'Microsoft.Security.Devops.Cli.linux-x64';
         }
+    } else {
+        packageName = 'Microsoft.Security.Devops.Cli';
     }
+    tl.debug(`packageName = ${packageName}`);
+    return packageName;
+}
 
-    isInstalled(versionsDirectory: string, cliVersion: string) : boolean {
-        let installed = false;
+/**
+ * Checks if the Guardian CLI is already installed
+ * 
+ * @param packagesDirectory - The directory where the Guardian CLI packages are installed
+ * @param packageName - The name of the Guardian CLI package to install
+ * @param cliVersion - The version of the Guardian CLI to install
+ * @returns true if the Guardian CLI is already installed, false otherwise
+ */
+function isInstalled(
+    packagesDirectory: string, 
+    packageName: string, 
+    cliVersion: string) : boolean {
+    let installed = false;
 
-        if (cliVersion.includes("*")) {
-            tl.debug(`MSDO CLI version contains a latest quantifier: ${cliVersion}. Continuing with install...`);
-            return installed;
-        }
-
-        this.setVariablesWithVersion(versionsDirectory, cliVersion);
-        
-        if (fs.existsSync(process.env.MSDO_DIRECTORY)) {
-            console.log(`MSDO CLI v${cliVersion} already installed.`);
-            installed = true;
-        }
-
+    if (common.isLatest(cliVersion)) {
+        tl.debug(`MSDO CLI version contains a latest quantifier: ${cliVersion}. Continuing with install...`);
         return installed;
     }
 
-    resolvePackageDirectory(
-        versionDirectory: string,
-        cliVersion: string) : void {
-        if (cliVersion.includes("*")) {
-            // find the latest directory
-            let packageDirectory = this.findLatestVersionDirectory(versionDirectory);
-            this.setVariables(packageDirectory);
-        } else {
-            this.setVariablesWithVersion(versionDirectory, cliVersion);
-        }
-
-        if (!fs.existsSync(process.env.MSDO_DIRECTORY)) {
-            throw `MSDO CLI v${cliVersion} was not found after installation.`
-        }
+    installed = setVariables(packagesDirectory, packageName, cliVersion);
+    
+    if (installed) {
+        console.log(`MSDO CLI v${cliVersion} already installed.`);
     }
 
-    findLatestVersionDirectory(versionsDirectory: string, isPreRelease: boolean = false) : string {
+    return installed;
+}
 
-        let latestDirectory = null;
-        let latestVersionParts = null;
-        let latestIsPreRelease = false;
-        let latestPreReleaseFlag = null;
+/**
+ * Sets the GDN_DIRECTORY and GDN_FILEPATH environment variables
+ * 
+ * @param packagesDirectory - The directory where the Guardian CLI packages are installed
+ * @param packageName - The name of the Guardian CLI package to install
+ * @param cliVersion - The version of the Guardian CLI to install
+ */
+function setVariables(
+    packagesDirectory: string, 
+    packageName: string, 
+    cliVersion: string, 
+    validate: boolean = false) : boolean {
 
-        // Get all of the directories in the versions directory
-        tl.debug(`Searching for all version folders in: ${versionsDirectory}`);
-        let dirs = this.getDirectories(versionsDirectory);
+    let packageDirectory = path.join(packagesDirectory, `${packageName}.${cliVersion}`);
+    tl.debug(`packageDirectory = ${packageDirectory}`);
 
-        // Evaluate each directory
-        for (let dirIndex = 0; dirIndex < dirs.length; dirIndex++) {
-            let dir = dirs[dirIndex];
+    let msdoDirectory = path.join(packageDirectory, 'tools');
+    tl.debug(`msdoDirectory = ${msdoDirectory}`);
 
-            if (dir == null || dir == "") {
-                tl.debug(`Skipping null or empty directory: ${dir}`);
-                continue;
-            }
+    let msdoFilePath = path.join(msdoDirectory, 'guardian');
+    tl.debug(`msdoFilePath = ${msdoFilePath}`);
 
-            tl.debug(`Evaluating MSDO directory: ${dir}`);
-            // If we reuse the same RegExp object, it will return null every other call
-            const dirRegex = new RegExp(/^(\d+\.?){1,6}(\-\w+)?$/g);
-            if (dirRegex.exec(dir) == null) {
-                tl.debug(`Skipping invalid version directory: ${dir}`);
-                continue;
-            }
+    process.env.MSDO_DIRECTORY = msdoDirectory;
+    process.env.MSDO_FILEPATH = msdoFilePath;
 
-            let fullVersionParts = dir.split("-");
+    let exists = fs.existsSync(process.env.MSDO_FILEPATH);
 
-            if (fullVersionParts == null || fullVersionParts.length < 0 || fullVersionParts.length > 2) {
-                tl.debug(`Skipping invalid version directory: ${dir}`);
-            }
-
-            let dirIsPreRelease = fullVersionParts.length > 1;
-
-            if (!isPreRelease && dirIsPreRelease) {
-                tl.debug(`Skipping pre-release version directory: ${dir}`);
-                continue;
-            }
-
-            let dirPreReleaseFlag = null;
-            if (dirIsPreRelease) {
-                dirPreReleaseFlag = fullVersionParts[1];
-            }
-
-            let versionNumbersString = fullVersionParts[0];
-
-            let versionParts = dir.split(".");
-
-            // If the latestDirectory isn't set yet, the folder is the latest directory
-            let isLatest = latestDirectory == null;
-
-            if (!isLatest) {
-                // Evaluate the directory's version against the latest directory
-
-                // Handle comparisions of separate level versions
-                // Some packages exclude Patch or include Revisions up to two levels (Rev1 and Rev2)
-                let maxVersionParts = versionParts.length;
-                if (latestVersionParts.length > maxVersionParts) {
-                    maxVersionParts = latestVersionParts.length;
-                }
-
-                for (let versionPartIndex = 0; versionPartIndex < versionParts.length; versionPartIndex++) {
-                    let versionPart = 0;
-                    let latestVersionPart = 0;
-
-                    let isLastVersionPart = versionPartIndex == (maxVersionParts - 1);
-
-                    if (versionPartIndex < versionParts.length) {
-                        versionPart = parseInt(versionParts[versionPartIndex]);
-                    }
-
-                    if (versionPartIndex < latestVersionParts.length) {
-                        latestVersionPart = parseInt(latestVersionParts[versionPartIndex]);
-                    }
-
-                    if (versionPart > latestVersionPart) {
-                        isLatest = true;
-                    } else if (versionPart == latestVersionPart) {
-                        isLatest = isLastVersionPart
-                            &&
-                            (
-                                (isPreRelease && latestIsPreRelease && dirPreReleaseFlag > latestPreReleaseFlag)
-                                ||
-                                (!isPreRelease && latestIsPreRelease)
-                            );
-                    } else {
-                        // Current version is less than latest found
-                        break;
-                    }
-
-                    if (isLatest) {
-                        break;
-                    }
-                }
-            }
-
-            if (isLatest) {
-                tl.debug(`Setting latest version directory: ${dir}`);
-                latestDirectory = path.join(versionsDirectory, dir);
-                latestVersionParts = versionParts;
-                latestIsPreRelease = dirIsPreRelease;
-                latestPreReleaseFlag = dirPreReleaseFlag;
-            }
-        }
-
-        tl.debug(`latestDirectory = ${latestDirectory}`);
-
-        return latestDirectory;
+    if (validate && !exists) {
+        throw new Error(`MSDO CLI v${cliVersion} was not found after installation. Expected location: ${msdoFilePath}`);
     }
 
-    getDirectories(directory: string) : string[] {
-        // read the directory for all paths
-        // filter for directories
-        return fs.readdirSync(directory).filter(p => this.isDirectory(directory, p));
-    }
-
-    isDirectory(directory: string, p: string) : boolean {
-        // statSync follows symlinks
-        return fs.statSync(path.join(directory, p)).isDirectory();
-    }
-
-    setVariablesWithVersion(versionDirectory: string, cliVersion: string) : void {
-        let packageDirectory = path.join(versionDirectory, cliVersion)
-        tl.debug(`packageDirectory = ${packageDirectory}`);
-
-        this.setVariables(packageDirectory);
-    }
-
-    setVariables(packageDirectory: string) : void {
-        let msdoDirectory = path.join(packageDirectory, 'tools');
-        tl.debug(`msdoDirectory = ${msdoDirectory}`);
-
-        let msdoFilePath = path.join(msdoDirectory, 'guardian');
-        tl.debug(`msdoFilePath = ${msdoFilePath}`);
-
-        process.env.MSDO_DIRECTORY = msdoDirectory;
-        process.env.MSDO_FILEPATH = msdoFilePath;
-    }
+    return exists;
 }
